2cf30215738a0fdf6d34f45d8ee5574b65ddaea0
Romain Semur <romain.semur@cdbdx.biz>
2023-03-16 13:35:01 +0100

    fix: 🐛 add https to retrieve tshirts

--- src/hooks/useTshirts/index.js
+++ src/hooks/useTshirts/index.js
@@ -3,7 +3,7 @@ import { useQuery } from "react-query";
 export default function useTshirts() {
   const queryResponse = useQuery("tshirts", () =>
     fetch(
-      "http://my-json-server.typicode.com/romainsemur/peak-tshirt-app/tshirts"
+      "https://my-json-server.typicode.com/romainsemur/peak-tshirt-app/tshirts"
     ).then((response) => response.json())
   );
 
