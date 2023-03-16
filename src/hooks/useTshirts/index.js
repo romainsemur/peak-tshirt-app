d0667da05523874bb6066a98c4b3a64bc1731dd5
Romain Semur <romain.semur@cdbdx.biz>
2023-03-16 13:15:27 +0100

    feat: 🎸 Add beautiful tshirt favicon

--- src/hooks/useTshirts/index.js
+++ src/hooks/useTshirts/index.js
@@ -2,7 +2,9 @@ import { useQuery } from "react-query";
 
 export default function useTshirts() {
   const queryResponse = useQuery("tshirts", () =>
-    fetch("http://localhost:3001/tshirts").then((response) => response.json())
+    fetch(
+      "http://my-json-server.typicode.com/romainsemur/peak-tshirt-app/tshirts"
+    ).then((response) => response.json())
   );
 
   return { ...queryResponse, tshirts: queryResponse.data };
