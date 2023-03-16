5770fc528adaae43a645022bdbc7b3244718d703
Romain Semur <romain.semur@cdbdx.biz>
2023-03-16 16:10:52 +0100

    fix: 🐛 update responseToJsonto to handle missing config.json

--- src/core/utils/responseToJson.js
+++ src/core/utils/responseToJson.js
@@ -1,3 +1,10 @@
-export default function responseToJson(response) {
-  return response.json();
+export default async function responseToJson(response) {
+  let data;
+  try {
+    data = await response.json();
+  } catch (error) {
+    data = {};
+  }
+
+  return data;
 }
--- src/index.js
+++ src/index.js
@@ -11,18 +11,17 @@ fetch("/config.json")
   .then((appConfig) => {
     config.set(appConfig);
   })
-  .then(() => {
-    import("./App").then((module) => {
-      const App = module.default;
-      const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      );
-      // If you want to start measuring performance in your app, pass a function
-      // to log results (for example: reportWebVitals(console.log))
-      // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-      reportWebVitals();
-    });
+  .then(() => import("./App"))
+  .then((module) => {
+    const App = module.default;
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    reportWebVitals();
   });
