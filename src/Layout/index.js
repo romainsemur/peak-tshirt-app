c4bf7731074005b621e4c5c27fb5eb66cc800a01
Romain Semur <romain.semur@cdbdx.biz>
2023-03-17 17:28:33 +0100

    feat: 🎸 update TshirtPage with some static content

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -9,16 +9,18 @@ import {
 } from "@mui/material";
 import { AddShoppingCart } from "@mui/icons-material";
 
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="h1">
-            Peak Shirt
-          </Typography>
+          <Link to="/">
+            <Typography variant="h6" component="h1">
+              Peak Shirt
+            </Typography>
+          </Link>
           {<Box sx={{ flexGrow: 1 }} />}
           <Box sx={{ display: { md: "flex" } }}>
             <IconButton
--- src/TshirtPage/index.js
+++ src/TshirtPage/index.js
@@ -1,3 +1,13 @@
+import TshirtCard from "TshirtCard";
+
 export default function TshirtPage() {
-  return <>TSHIRT PAGE</>;
+  const tshirt = {
+    id: 1,
+    name: "Tshirt",
+    description: "",
+    imageUrl: "1.jpg",
+    size: "M",
+    price: 12.99,
+  };
+  return <TshirtCard {...tshirt} />;
 }
