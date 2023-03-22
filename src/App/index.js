631779f4295cc51d9201b410e8a2a5f4a8142104
Romain Semur <romain.semur@cdbdx.biz>
2023-03-22 10:47:26 +0100

    fix: 🐛 fix ErrorPage position

--- src/App/index.js
+++ src/App/index.js
@@ -27,11 +27,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <HomePage />,
+        errorElement: <ErrorPage />,
       },
       {
         path: "tshirts/:tshirtId",
